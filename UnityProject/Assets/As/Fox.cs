657fe7bdc0ce0c75997525f188fa535c96e70085
35752987zxc <57181192+35752987zxc@users.noreply.github.com>
2019-11-20 18:13:54 +0800

    1111

--- UnityProject/Assets/As/Fox.cs
+++ UnityProject/Assets/As/Fox.cs
@@ -4,11 +4,39 @@ public class Fox : MonoBehaviour
 {   [Header("移動速度"),Range(0.1f,999f)]
     public float speed = 1f;
 
+
+    public Rigidbody2D ri;
+
+    public SpriteRenderer sp;
+
+
+
+
+
+
+
+
     /// <summary>
     /// 移動
     /// </summary>
-    private void speedd()
+    private void spee()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+    }
+
+
+    private void Update()
     {
 
+
+
+       
+
     }
+
+
+
+
+
+
 }
