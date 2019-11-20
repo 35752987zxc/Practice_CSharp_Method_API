a032989db0ecfabbdd7f152fb7a8d1529e078b3f
35752987zxc <57181192+35752987zxc@users.noreply.github.com>
2019-11-20 10:11:47 +0800

    0001

--- UnityProject/Assets/As/Fox.cs
+++ UnityProject/Assets/As/Fox.cs
@@ -3,4 +3,12 @@
 public class Fox : MonoBehaviour
 {   [Header("移動速度"),Range(0.1f,999f)]
     public float speed = 1f;
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void speedd()
+    {
+
+    }
 }
